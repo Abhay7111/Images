5dabfc86112146ed53cd6643e9410467cb1b30a9
Abhay7111 <gaamaofficial@gmail.com>
2023-09-11 14:21:44 +0530

    second page

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ gsap.to("#header",{
         top:"120px",
     },
 })
+
+var crsr = document.querySelector(".cur-t")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
+
+var crsrblr = document.querySelector(".cur-b")
+document.addEventListener("mousemove",function(dets){
+    crsrblr.style.left = dets.x -100 +"px"
+    crsrblr.style.top = dets.y -100 +"px"
+})
