d209fc05789adf893c3d51f50b31056f8c36766c
abhay vishwakarma <gaamaofficial@gmail.com>
2023-10-05 15:51:14 +0530

    code

---
+++ page3/Collage-Friends/script.js
@@ -0,0 +1,102 @@
+var btn1 = document.querySelector("#button1")
+var btn2 = document.querySelector("#button2")
+var btn3 = document.querySelector("#button3")
+var btn4 = document.querySelector("#button4")
+var btn5 = document.querySelector("#button5")
+var btn6 = document.querySelector("#button6")
+var btn7 = document.querySelector("#button7")
+var btn8 = document.querySelector("#button8")
+var btn9 = document.querySelector("#button9")
+var btn10 = document.querySelector("#button10")
+var btn11 = document.querySelector("#button11")
+var btn12 = document.querySelector("#button12")
+
+var disnone = document.querySelector("#disnone")
+
+var submit = document.querySelector("#submit")
+
+var bt1 = document.querySelector("#bt1")
+var bt2 = document.querySelector("#bt2")
+var bt3 = document.querySelector("#bt3")
+var bt4 = document.querySelector("#bt4")
+var bt5 = document.querySelector("#bt5")
+var bt6 = document.querySelector("#bt6")
+var bt7 = document.querySelector("#bt7")
+var bt8 = document.querySelector("#bt8")
+var bt9 = document.querySelector("#bt9")
+var bt10 = document.querySelector("#bt10")
+var bt11 = document.querySelector("#bt11")
+var bt12 = document.querySelector("#bt12")
+
+btn1.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt1.innerHTML = "1"
+})
+btn2.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt2.innerHTML = "2"
+})
+btn3.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt3.innerHTML = "3"
+})
+btn4.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt4.innerHTML = "4"
+})
+btn5.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt5.innerHTML = "5"
+})
+btn6.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt6.innerHTML = "6"
+})
+btn7.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt7.innerHTML = "7"
+})
+btn8.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt8.innerHTML = "8"
+})
+btn9.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt9.innerHTML = "9"
+})
+btn10.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt12.style.display = "0"
+     bt10.innerHTML = "* Not included sorry (Refresh again)"
+     bt1.style.display = "none"
+     bt2.style.display = "none"
+     bt3.style.display = "none"
+     bt4.style.display = "none"
+     bt5.style.display = "none"
+     bt6.style.display = "none"
+     bt7.style.display = "none"
+     bt8.style.display = "none"
+     bt9.style.display = "none"
+     bt11.style.display = "none"
+
+})
+btn11.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt11.innerHTML = "0"
+})
+btn12.addEventListener("click",function(){
+     disnone.style.display = "none"
+     bt10.style.display = "none"
+     bt12.innerHTML = "# Not included sorry (Refresh again)"
+     bt1.style.display = "none"
+     bt2.style.display = "none"
+     bt3.style.display = "none"
+     bt4.style.display = "none"
+     bt5.style.display = "none"
+     bt6.style.display = "none"
+     bt7.style.display = "none"
+     bt8.style.display = "none"
+     bt9.style.display = "none"
+     bt11.style.display = "none"
+
+})
